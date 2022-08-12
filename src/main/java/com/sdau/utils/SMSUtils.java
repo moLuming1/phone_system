465c497b40acc745976988a418fe2741d1591b69
mlm <1782481254@qq.com>
2022-08-12 16:36:15 +0800

    课堂管理系统8月12号

--- src/main/java/com/sdau/utils/SMSUtils.java
+++ src/main/java/com/sdau/utils/SMSUtils.java
@@ -21,7 +21,7 @@ public class SMSUtils {
      * @param param        参数
      */
     public static void sendMessage(String signName, String templateCode, String phoneNumbers, String param) {
-        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI5tEGfardQj7JMC7uxm8y", "cdLTiBGoaOxnN1jtCZwSNhXoLBsaQd");
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "", "");
         IAcsClient client = new DefaultAcsClient(profile);
 
         SendSmsRequest request = new SendSmsRequest();
